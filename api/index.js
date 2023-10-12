1845d9aadfb22da52e8feeff9f084b7bdda5b43d
Sahil <ss6611786@gmail.com>
2023-10-12 14:20:02 +0530

    our database connection is established successfully

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
+
+
+// mongodb connection
+mongoose.connect(process.env.mongouri)
+    .then(() => console.log('mongodb connected successfully'))
+    .catch((error) => console.log(error))
+
 
 const app = express();
 
