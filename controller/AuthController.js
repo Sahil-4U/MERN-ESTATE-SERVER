f2aa15199fd4b6f27a9472d71514f7d88a0ce11d
Sahil <ss6611786@gmail.com>
2023-10-18 10:36:33 +0530

    I have created a middle ware to controll the error handling

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,7 +1,7 @@
 import User from "../modals/User.modal.js";
 import bcryptjs from 'bcryptjs';
 
-export const singup = async (req, res) => {
+export const singup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const userDB = new User({ username, email, password: hashedPassword });
@@ -9,6 +9,6 @@ export const singup = async (req, res) => {
         await userDB.save();
         res.status(201).json("User created successfully!!!");
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
-}
+};
