96a65a00f8c208801e7fb638e9d263632a7a20a7
Sahil <ss6611786@gmail.com>
2023-10-20 14:32:19 +0530

    we have successfully created sign-in controller

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,6 +1,7 @@
 import User from "../modals/User.modal.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/Error.js";
+import jwt from 'jsonwebtoken';
 
 export const singup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -14,9 +15,19 @@ export const singup = async (req, res, next) => {
     }
 };
 export const signin = async (req, res, next) => {
-    const { email, password } = req.body;
-    const emailValidate = await User.findOne({ email });
-    if (!emailValidate) return next(errorHandler(404, 'User not found please Sing Up first'));
-    const comPassword = bcryptjs.compareSync(password, emailValidate.password);
-    if (!comPassword) return next(errorHandler(404, 'Wrong Credentials'));
+    try {
+       const { email, password } = req.body;
+       const Validater = await User.findOne({ email });
+       if (!Validater) return next(errorHandler(404, 'User not found please Sing Up first'));
+       const comPassword = bcryptjs.compareSync(password, Validater.password);
+       if (!comPassword) return next(errorHandler(404, 'Wrong Credentials'));
+
+       const token = jwt.sign({ id: Validater._id }, process.env.JWT_SECRET);
+
+       const { password: pass, ...rest } = Validater._doc;
+
+       res.cookie('access token', token, { httpOnly: true }).status(200).json(rest);
+   } catch (error) {
+       next(error);
+   }
 }
--- routes/Auth.routes.js
+++ routes/Auth.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { singup } from '../controller/AuthController.js';
+import { signin, singup } from '../controller/AuthController.js';
 
 const authRouter = express.Router();
 
 authRouter.post("/sing-up", singup);
+authRouter.post("/sign-in", signin);
 
 export default authRouter;
