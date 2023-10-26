39553b609c79a1875ba1864b6e4ffa022dbbba2e
Sahil <ss6611786@gmail.com>
2023-10-26 12:33:05 +0530

    so I have complete the backend part for googleauth

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -35,3 +35,27 @@ export const signin = async (req, res, next) => {
         next(error);
     }
 };
+
+export const google = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (user) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { password: pass, ...rem } = user._doc;
+            return res.cookie(`access_token`, token, { httpOnly: true }).status(200).json(rem);
+        } else {
+            const password = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPassword = await bcryptjs.hashSync(password, 10);
+            const newUserDB = new User({ email: req.body.email, password: hashedPassword, username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), avatar: req.body.photo })
+            const result = await newUserDB.save();
+            const token = jwt.sign({ id: result._id }, process.env.JWT_SECRET);
+            const { password: pass, ...rest } = result._doc;
+            return res
+                .cookie(`access_token`, token, { httpOnly: true })
+                .status(200)
+                .json(rest);
+        }
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/Auth.routes.js
+++ routes/Auth.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signin, singup } from '../controller/AuthController.js';
+import { google, signin, singup } from '../controller/AuthController.js';
 
 const authRouter = express.Router();
 
 authRouter.post("/sing-up", singup);
 authRouter.post("/sign-in", signin);
+authRouter.post('/google', google)
 
 export default authRouter;
