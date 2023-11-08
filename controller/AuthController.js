4ccc5053f77f6841e08574521254782e02f4892c
Sahil <ss6611786@gmail.com>
2023-11-08 11:33:11 +0530

    some improvements to solve cookie issue

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -25,10 +25,12 @@ export const signin = async (req, res, next) => {
         const token = jwt.sign({ id: Validater._id }, process.env.JWT_SECRET);
 
         const { password: pass, ...rest } = Validater._doc;
-        console.log(token, rest, "line 28 at authcontoller.js");
 
-        res
-            .cookie(`access_token`, token, { httpOnly: true })
+
+        return res.cookie(`access_token`, token, {
+            expires: new Date(Date.now() + 30000),
+            httpOnly: true
+        })
             .status(200)
             .json(rest);
     } catch (error) {
